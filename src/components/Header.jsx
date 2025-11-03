15e91e0f1ba5e27251994395d4e3007613029b09
LieuTuanKiet <Redboy71784@gmail.com>
2025-11-03 19:44:52 +0700

    feat:Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import ICON from "../assets/ICON-Logo.png";
+export default function Header(){
+    const [active,setActive] = useState("introduce");
+    return(
+        <div className="w-screen sm:w-full backdrop-blur-md bg-white/80 sticky top-0 z-50">
+            <div className="container mx-auto flex gap-x-4 items-center">
+                <div className="max-w-12 max-h-12">
+                    <img src={ICON} alt="Icon-Logo" />
+                </div>
+                <p className="w-2xl font-bold text-xl text-primary-color">ICON CLUB</p>
+                {/*<div className="flex justify-center items-center gap-x-4 w-full">
+                    <a href="#" 
+                        className={`${active==="introduce"?"bg-gray-300/30 text-primary-color!":"bg-gray-100/20 text-secondary-color/50!"} p-2 w-full text-center max-w-[90px] max-h-12 rounded-lg hover:text-primary-color/70! hover:bg-gray-200/50 duration-200`} 
+                        onClick={() => setActive("introduce")}>
+                            20/11
+                    </a>
+                    <a href="#" 
+                        className={`${active==="gallery"?"bg-gray-300/30 text-primary-color!":"bg-gray-100/20 text-secondary-color/50!"} p-2 w-full text-center max-w-[90px] max-h-12 rounded-lg hover:text-primary-color/70! hover:bg-gray-200/50 duration-200`} 
+                        onClick={() => setActive("gallery")}>
+                            Trưng bày
+                    </a>
+                    <a href="#" 
+                        className={`${active==="contact"?"bg-gray-300/30 text-primary-color!":"bg-gray-100/20 text-secondary-color/50!"} p-2 w-full text-center max-w-[90px] max-h-12 rounded-lg hover:text-primary-color/70! hover:bg-gray-200/50 duration-200`} 
+                        onClick={() => setActive("contact")}>
+                            Liên hệ
+                    </a>
+                </div>*/}
+            </div>
+        </div>
+    )
+}
